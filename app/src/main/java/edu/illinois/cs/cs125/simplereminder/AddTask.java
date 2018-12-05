2118344bc5025298025fe5e006c5f0fc7e8634da
zhangyiqi <yiqiz2@illinois.edu>
2018-12-05 14:55:55 -0600

    1

---
+++ app/src/main/java/edu/illinois/cs/cs125/simplereminder/AddTask.java
@@ -0,0 +1,13 @@
+package edu.illinois.cs.cs125.simplereminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_task);
+    }
+}
