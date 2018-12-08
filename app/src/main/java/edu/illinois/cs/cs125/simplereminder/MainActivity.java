bed64f5269a41568b7a8bb91a41f5cb286210f30
int0thewind <yinhanzhi@gmail.com>
2018-12-08 15:19:47 -0600

    MainActivity Modified

--- app/src/main/java/edu/illinois/cs/cs125/simplereminder/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/simplereminder/MainActivity.java
@@ -1,16 +1,32 @@
 package edu.illinois.cs.cs125.simplereminder;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String DEBUG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
+        final Button addTask = findViewById(R.id.addTask);
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent jumpToAddTaskActivity = new Intent(MainActivity.this, AddTask.class);
+                startActivity(jumpToAddTaskActivity);
+                Log.d(DEBUG_TAG, "float button on the Main Activity clicked");
+            }
+        });
 
     }
 }
