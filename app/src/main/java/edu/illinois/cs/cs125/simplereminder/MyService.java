51edc322d0437feb41366e04bb614003c591cff1
Hanzhi Yin <hanzhiy2@illinois.edu>
2018-12-10 14:10:05 -0600

    New blank service added!

--- app/src/main/java/edu/illinois/cs/cs125/simplereminder/MyService.java
+++ app/src/main/java/edu/illinois/cs/cs125/simplereminder/MyService.java
@@ -3,9 +3,16 @@ package edu.illinois.cs.cs125.simplereminder;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+
+import edu.illinois.cs.cs125.lib.*;
 
 public class MyService extends Service {
+
+    private static final String CHANNEL_ID = "simpleReminderNotification";
+
     public MyService() {
+        NotificationCompat.Builder = new NotificationCompat.Builder(this, CHANNEL_ID).setContentTitle("You have a")
     }
 
     @Override
